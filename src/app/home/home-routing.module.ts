4ff890acc577c02348eb51a944bda5a275accbe4
laZZySpiDer <adityapandya7001@gmail.com>
2022-09-02 11:53:40 +0530

    login page added, functionality remaining

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { WallDisplayComponent } from './pages/wall-display/wall-display.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'wall-display', component: WallDisplayComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,6 +21,34 @@ export class HomeComponent implements OnInit {
     "../../assets/images/3.jpg",
     "../../assets/images/4.jpg",
   ]
+
+
+  characteristicList = [
+    {
+      id: 1,
+      heading : 'Ready to Ship within 24 hours',
+      content : ' Over 100 custom products are ready to ship within 24 hours.',
+      icon : 'local_shipping'
+    },
+    {
+      id: 2,
+      heading : 'Now Offering Buy Now, Pay Later',
+      content : 'Pay in 4 interest - free payments on purchase of 500+ INR.',
+      icon : 'payment'
+    },
+    {
+      id: 3,
+      heading : 'Over 600000 Prints Sold',
+      content : `Since 2015, we've sold 600000 photo gifts to the worldwide.`,
+      icon : 'shopping_basket'
+    },
+    {
+      id: 4,
+      heading : '100% Love-it Guarantee',
+      content : 'Love it or send it back! We guarantee a full refund with no hassle.',
+      icon : 'check_circle'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
