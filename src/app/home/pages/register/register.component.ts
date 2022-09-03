7eec503141bb315b3e5683b77b82715663e1d139
laZZySpiDer <adityapandya7001@gmail.com>
2022-09-03 14:05:07 +0530

    registration flow completed

--- src/app/home/pages/register/register.component.ts
+++ src/app/home/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl, ValidatorFn, AbstractControl, ValidationErrors, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { LoginService } from 'src/app/services/login.service';
 import { CustomValidators } from './../../../providers/CustomValidators';
 
 @Component({
@@ -19,13 +21,31 @@ export class RegisterComponent implements OnInit {
   },
    CustomValidators.mustMatch('password','confirmPassword')
   );
-  constructor(private toastr: ToastrService, private fb:FormBuilder) { }
+  constructor(private toastr: ToastrService, private fb:FormBuilder,private loginService: LoginService,private _router:Router) { }
 
   ngOnInit(): void {
   }
 
-  checkToastr(){
-    this.toastr.success('Hello Toastr','Message');
+  registerUser(){
+    // this.toastr.success('Hello Toastr','Message');
+    if(this.registrationForm.invalid){
+      this.toastr.error('Please resolve all Errors before Submitting','Register User');
+      return;
+    }
+    let userData = {
+      firstName: this.registrationForm.value.firstName,
+      lastName:  this.registrationForm.value.lastName,
+      email: this.registrationForm.value.emailId,
+      password: this.registrationForm.value.password,
+      phone:this.registrationForm.value.phoneNumber
+    }
+    console.log(this.registrationForm.value);
+    this.loginService.registerUser(userData)
+    .subscribe((response:any)=>{
+      console.log(response);
+      this.toastr.success('User Registered','Register User');
+      this._router.navigate(['']);
+    })
   }
 
 
--- src/app/providers/CustomValidators.ts
+++ src/app/providers/CustomValidators.ts
@@ -8,19 +8,6 @@ import {
   
   export class CustomValidators {
     constructor() {}
-  
-    static onlyChar(): ValidatorFn {
-      return (control: AbstractControl): { [key: string]: boolean } | null => {
-        if (control.value == '') return null;
-  
-        let re = new RegExp('^[a-zA-Z ]*$');
-        if (re.test(control.value)) {
-          return null;
-        } else {
-          return { onlyChar: true };
-        }
-      };
-    }
     static mustMatch(controlName: string, matchingControlName: string) {
       return (formGroup: AbstractControl): ValidationErrors | null => {
         const control = formGroup.get(controlName);
